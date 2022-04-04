7b82c36820ad2880270e2d625f2982227f1abfa1
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-04 18:53:48 +1000

    convert Pydantic's Model to dict

--- backend/crud/base.py
+++ backend/crud/base.py
@@ -37,8 +37,7 @@ class CRUDBase(
     def create(
         self, db: Session, *, obj_in: CreateSchemaType
     ) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data)  # type: ignore
+        db_obj = self.model(**dict(obj_in))  # type: ignore
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
