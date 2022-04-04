5e89fde411a810a91a309f812363d989f03ccefb
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-04 18:55:10 +1000

    rewrite user's routes from sqlmodel to sqlalchemy

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi_azure_auth.user import User as UserAzure
 
-# from backend.routers.dependencies import (
-#     user_exist, get_user_azure
-# )
 from backend.resources import strings
+from backend.schemas.user import (
+    UserBase, UserInDBBase
+)
+from backend.routers.dependencies import (
+    get_db, user_exist, get_user_azure
+)
+from backend import crud
 
 router = APIRouter(
     prefix="/user",
@@ -12,61 +15,60 @@ router = APIRouter(
 )
 
 
-# @router.get(
-#     '',
-#     name="user:get",
-#     description="Get user by id",
-#     response_model=User,
-# )
-# def get_user(
-#     user_id: int,
-# ):
-#     with Session(engine) as session:
-#         user = session.get(User, user_id)
-#         if not user:
-#             raise HTTPException(
-#                 status_code=404,
-#                 detail=strings.USER_DOES_NOT_FOUND_ERROR
-#             )
-#         return user
-#
-#
-# @router.get(
-#     "/me",
-#     name="user:getme",
-#     description="Get current profile information",
-#     response_model=User,
-#     dependencies=[Depends(user_exist)],
-# )
-# def get_me(
-#     request: Request,
-# ):
-#     return request.state.current_user
-#
-#
-# @router.get(
-#     "/register",
-#     name="user:register",
-#     response_model=UserCreate,
-# )
-# async def register(
-#     user_azure: UserAzure = Depends(get_user_azure),
-#     session: Session = Depends(get_session),
-# ):
-#     with Session(engine) as session:
-#         users = session.exec(
-#             select(User).where(User.email == user_azure.email)
-#         ).all()
-#         if users:
-#             raise HTTPException(
-#                 status_code=400,
-#                 detail=strings.USER_WITH_THIS_EMAIL_ALREADY_EXIST_ERROR
-#             )
-#         else:
-#             user = User(
-#                 name=user_azure.name,
-#                 email=user_azure.email
-#             )
-#             session.add(user)
-#             session.commit()
-#             return user_azure
+@router.get(
+    '/all',
+)
+def get_usets(*, db=Depends(get_db)):
+    return crud.user.get_multi(db)
+
+
+@router.get(
+    '/get/{user_id}',
+    name="user:get",
+    description="Get user by id",
+    response_model=UserInDBBase,
+)
+def get_user(
+    user_id: int,
+    db=Depends(get_db),
+):
+    user = crud.user.get(db, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail=strings.USER_DOES_NOT_FOUND_ERROR
+        )
+    return user
+
+
+@router.get(
+    "/me",
+    name="user:getme",
+    description="Get current profile information",
+    response_model=UserInDBBase,
+    dependencies=[Depends(user_exist)],
+)
+def get_me(
+    request: Request,
+    db=Depends(get_db),
+):
+    return request.state.current_user
+
+
+@router.get(
+    "/register",
+    name="user:register",
+    response_model=UserBase,
+)
+async def register(
+    user_azure: UserBase = Depends(get_user_azure),
+    db=Depends(get_db),
+):
+    user = crud.user.get_by_email(db, user_azure.email)
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail=strings.USER_WITH_THIS_EMAIL_ALREADY_EXIST_ERROR
+        )
+    crud.user.create(db, obj_in=user_azure)
+    return user_azure
