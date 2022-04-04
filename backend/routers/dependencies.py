232384738d772fb1662e110394fea2001cf641e2
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-04 18:54:52 +1000

    rewrite dependecies from sqlmodel to sqlalchemy

--- backend/routers/dependencies.py
+++ backend/routers/dependencies.py
@@ -1,12 +1,15 @@
 from typing import Generator
 from fastapi import Request, Depends, HTTPException
 
+from sqlalchemy.orm import Session
+
 from fastapi_azure_auth.user import User as UserAzure
 
-from backend.models.user import User
+from backend.schemas.user import UserBase
 from backend.routers.authentication import azure_scheme
 from backend.resources import strings
 from backend.database.session import SessionLocal
+from backend import crud
 
 
 def get_db() -> Generator:
@@ -16,29 +19,25 @@ def get_db() -> Generator:
     finally:
         db.close()
 
-# def get_user_info_from_schema(user: UserAzure):
-#     user_process = {
-#         "email": user.claims["preferred_username"],
-#         "name": user.claims["name"]
-#     }
-#     return UserBase(**user_process)
-#
-#
-# def get_user_azure(user_azure: UserAzure = Depends(azure_scheme)):
-#     return get_user_info_from_schema(user_azure)
-#
-#
-# def user_exist(
-#     request: Request,
-#     user_azure: UserAzure = Depends(get_user_azure),
-# ):
-#     with Session(engine) as session:
-#         user: User | None = session.exec(
-#             select(User).where(User.email == user_azure.email)
-#         ).first()
-#         if not user:
-#             raise HTTPException(
-#                 status_code=400,
-#                 detail=strings.USER_DOES_NOT_EXIST_ERROR
-#             )
-#         request.state.current_user = user
+
+def get_user_azure(
+    user_azure: UserAzure = Depends(azure_scheme)
+) -> UserBase:
+    return UserBase(
+        email=user_azure.claims["preferred_username"],
+        name=user_azure.claims["name"],
+    )
+
+
+def user_exist(
+    request: Request,
+    user_azure: UserAzure = Depends(get_user_azure),
+    db: Session = Depends(get_db),
+):
+    user = crud.user.get_by_email(db, user_azure.email)
+    if not user:
+        raise HTTPException(
+            status_code=400,
+            detail=strings.USER_DOES_NOT_EXIST_ERROR
+        )
+    request.state.current_user = user
