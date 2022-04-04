b03b68a752bb0572260a3b41ef5c913be74c74eb
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-04 17:37:28 +1000

    create crud user

---
+++ backend/crud/user.py
@@ -0,0 +1,17 @@
+from typing import Optional, Any
+
+from sqlalchemy.orm import Session
+
+from backend.crud.base import CRUDBase
+from backend.models.user import User
+from backend.schemas.user import UserCreate, UserUpdate
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+
+    def get_by_email(self, db: Session, email: Any) -> Optional[User]:
+        return db.query(self.model).\
+            filter(self.model.email == email).first()
+
+
+user = CRUDUser(User)
