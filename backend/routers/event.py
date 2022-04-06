df0cbaa3a5f7e6353bc2b8a68886b2d0a76b7efc
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-07 04:11:35 +1000

    get, update, delete endpoints for event

--- backend/routers/event.py
+++ backend/routers/event.py
@@ -1,10 +1,10 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, Request
 
 from backend import crud
-from backend.resources import strings
-from backend.routers.dependencies import get_db, get_user_azure, user_exist
-from backend.schemas.user import UserAzure, UserUpdate, UserInDBBase
-from backend.schemas.event import EventBase, EventCreate, EventInDBBase
+from backend.routers.dependencies import get_db, user_exist
+from backend.schemas.event import EventCreate, EventUpdate, EventInDBBase
 
 
 router = APIRouter(
@@ -14,7 +14,7 @@ router = APIRouter(
 
 
 @router.post(
-    "/create",
+    "/",
     response_model=EventInDBBase,
     dependencies=[Depends(user_exist)],
 )
@@ -25,3 +25,84 @@ def create_event(
 ):
     return crud.event.create_with_user(
         db, obj_in=event_in, user_id=request.state.current_user.id)
+
+
+@router.put(
+    "/{event_id}",
+    response_model=EventInDBBase,
+    dependencies=[Depends(user_exist)],
+)
+def update_event(
+    request: Request,
+    event_id: int,
+    event_in: EventUpdate,
+    db=Depends(get_db),
+):
+    event = crud.event.get(db, id=event_id)
+
+    if not event:
+        raise HTTPException(
+            status_code=404
+        )
+
+    if request.state.current_user.id != event.user_id:
+        raise HTTPException(
+            status_code=403
+        )
+    return crud.event.update(db=db, db_obj=event, obj_in=event_in)
+
+
+@router.delete(
+    "/{event_id}",
+    response_model=EventInDBBase,
+    dependencies=[Depends(user_exist)],
+)
+def delete_event(
+    request: Request,
+    event_id,
+    db=Depends(get_db),
+):
+    event = crud.event.get(db, id=event_id)
+
+    if not event:
+        raise HTTPException(
+            status_code=404
+        )
+
+    if not request.state.current_user.is_admin and\
+            not request.state.current_user.is_moderator and\
+            (request.state.current_user.id != event.user_id):
+        raise HTTPException(
+            status_code=403
+        )
+    return crud.event.remove(db=db, id=event_id)
+
+
+@router.get(
+    "/{event_id}",
+    response_model=EventInDBBase,
+)
+def get_event(
+    event_id: int,
+    db=Depends(get_db),
+):
+    event = crud.event.get(db, id=event_id)
+
+    if not event:
+        raise HTTPException(
+            status_code=404
+        )
+    return event
+
+
+@router.get(
+    "/",
+    response_model=List[EventInDBBase],
+)
+def get_events(
+    skip: int = 0,
+    limit: int = 100,
+    db=Depends(get_db)
+):
+    events = crud.event.get_multi(db, skip=skip, limit=limit)
+    return events
