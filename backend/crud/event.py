87d125b4d4ba7d9827b59b4ba7235593de763cd6
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-07 17:26:56 +1000

    CRUDEvent add method get_multi_with_filter

--- backend/crud/event.py
+++ backend/crud/event.py
@@ -1,4 +1,5 @@
-from typing import Any, Optional
+from typing import Any, Optional, List
+from datetime import datetime
 
 from sqlalchemy.orm import Session
 
@@ -17,6 +18,32 @@ class CRUDEvent(CRUDBase[Event, EventCreate, EventUpdate]):
         db.commit()
         db.refresh(db_obj)
         return db_obj
+
+    def get_multi_with_filter(
+        self, db, skip, limit, title, date_begin: datetime,
+        date_end: datetime, user_id, tags: List[str]
+    ) -> List[Event]:
+        query = db.query(self.model)
+
+        if title:
+            query = query.filter(Event.title.contains(title))
+
+        if date_begin:
+            query = query.filter(Event.date_begin >= date_begin)
+
+        if date_end:
+            query = query.filter(Event.date_begin <= date_end)
+
+        if user_id:
+            query = query.filter(Event.user_id == user_id)
+
+        if tags:
+            query = query.filter(Event.tags.contains(tags))
+
+        return query.\
+            order_by(Event.date_begin.desc(), Event.id.desc()).\
+            offset(skip).limit(limit).\
+            all()
 
 
 event = CRUDEvent(Event)
