5526fbaa55c875031ad0f5676060ab02cde93b1f
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-09 03:03:55 +1000

    add category router to app

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from backend.routers.me import router as me_router
 from backend.routers.personal_events import router as\
     personal_events_router
 from backend.routers.place import router as place_router
+from backend.routers.category import router as category_router
 
 app = FastAPI(
     swagger_ui_oauth2_redirect_url='/oauth2-redirect',
@@ -45,3 +46,4 @@ app.include_router(user_router)
 app.include_router(event_router)
 app.include_router(personal_events_router)
 app.include_router(place_router)
+app.include_router(category_router)
