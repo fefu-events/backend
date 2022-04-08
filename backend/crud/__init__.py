395c8cb2a60e3ce3fa5720993d758f3dc44b8ab2
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-09 03:02:05 +1000

    add category

--- backend/crud/__init__.py
+++ backend/crud/__init__.py
@@ -1,3 +1,4 @@
 from backend.crud.event import event  # noqa
 from backend.crud.user import user  # noqa
 from backend.crud.place import place # noqa
+from backend.crud.category import category # noqa
--- backend/database/base.py
+++ backend/database/base.py
@@ -4,3 +4,4 @@ from backend.database.base_class import Base  # noqa
 from backend.models.event import Event #noqa
 from backend.models.user import User #noqa
 from backend.models.place import Place #noqa
+from backend.models.category import Category #noqa
