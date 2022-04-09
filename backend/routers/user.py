48a95c9f63abe26768444c431249f7eb0b50bcd0
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-10 01:05:09 +1000

    endpoints for follow/unfollow

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -4,6 +4,10 @@ from backend import crud
 from backend.resources import strings
 from backend.routers.dependencies import get_db, get_user_azure, user_exist
 from backend.schemas.user import UserAzure, UserInDBBase, UserUpdate
+from backend.schemas.user_subscription import (
+    UserSubscriptionCreate, UserSubscriptionInDBBase
+)
+
 
 router = APIRouter(
     prefix="/user",
@@ -40,3 +44,60 @@ def get_user(
             detail=strings.USER_DOES_NOT_FOUND_ERROR
         )
     return user
+
+
+@router.post(
+    '/{user_id}/follow',
+    name="user:follow_by_id",
+    response_model=UserSubscriptionInDBBase,
+    dependencies=[Depends(user_exist)],
+)
+def follow_user(
+    request: Request,
+    user_id: int,
+    db=Depends(get_db),
+):
+    if user_id == request.state.current_user.id:
+        raise HTTPException(
+            status_code=400,
+        )
+    user = crud.user.get(db, user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail=strings.USER_DOES_NOT_FOUND_ERROR
+        )
+    create_data = {
+        'user_id': user_id,
+        'follower_id': request.state.current_user.id
+    }
+    user_subscription = crud.user_subscription.get_by_users(
+        db, **create_data)
+    if user_subscription:
+        raise HTTPException(
+            status_code=400,
+        )
+    return crud.user_subscription.create(
+        db, obj_in=UserSubscriptionCreate(**create_data))
+
+
+@router.delete(
+    '/{user_id}/unfollow',
+    name="user:unfollow_by_id",
+    response_model=UserSubscriptionInDBBase,
+    dependencies=[Depends(user_exist)],
+)
+def unfollow_user(
+    request: Request,
+    user_id: int,
+    db=Depends(get_db),
+):
+    user_subscription = crud.user_subscription.get_by_users(
+        db, user_id=user_id,
+        follower_id=request.state.current_user.id)
+    if not user_subscription:
+        raise HTTPException(
+            status_code=400,
+        )
+    return crud.user_subscription.remove(
+        db, id=user_subscription.id)
