c14e26cdec66830c45969c4eff9e7aff61f336f9
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-10 20:13:30 +1000

    participation crud

--- backend/crud/__init__.py
+++ backend/crud/__init__.py
@@ -3,3 +3,4 @@ from backend.crud.event import event  # noqa
 from backend.crud.place import place  # noqa
 from backend.crud.user import user  # noqa
 from backend.crud.user_subscription import user_subscription  # noqa
+from backend.crud.participation import participation # noqa
---
+++ backend/crud/participation.py
@@ -0,0 +1,32 @@
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+
+from backend.crud.base import CRUDBase
+from backend.models.participation import Participation
+from backend.schemas.participation import ParticipationCreate,\
+    ParticipationInDBBase
+
+
+class CRUDParticipation(
+    CRUDBase[Participation, ParticipationCreate, None]
+):
+
+    def get_by_event_and_user(
+        self, db: Session, event_id: int, user_id: int
+    ) -> Participation | None:
+        return db.query(Participation).\
+            filter(and_(Participation.event_id == event_id,
+                        Participation.user_id == user_id)).first()
+
+    def create_with_user(
+        self, db: Session, *, obj_in: ParticipationCreate,
+        user_id: int
+    ) -> Participation:
+        db_obj = Participation(**dict(obj_in), user_id=user_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+
+participation = CRUDParticipation(Participation)
