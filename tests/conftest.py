199fd002b42edc4616a10f62ec1134c916c3a2db
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-11 02:52:26 +1000

    conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+from backend.database.session_test import SessionLocal, engine
+from backend.database.base import Base
+
+
+@pytest.fixture(scope="session")
+def db():
+    Base.metadata.create_all(engine)
+    session = SessionLocal()
+    yield session
+    session.close()
+    Base.metadata.drop_all(engine)
