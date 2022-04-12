655aef4e262b7044ba6134c39864c1493f55dbf8
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-12 17:35:46 +1000

    test/utils.py for date

--- tests/utils.py
+++ tests/utils.py
@@ -25,3 +25,25 @@ def get_ids_ordered(items):
     result = [item.id for item in items]
     result.sort()
     return result
+
+
+def get_date_now() -> datetime:
+    return datetime.now(timezone.utc)
+
+
+def get_date_now_offset(
+    years: int = 0, months: int = 0, days: int = 0, hours: int = 0
+) -> datetime:
+    return get_date_now() + relativedelta(
+        years=years, months=months, days=days, hours=hours)
+
+
+def get_date_now_offset_str(
+    years: int = 0, months: int = 0, days: int = 0, hours: int = 0
+) -> datetime:
+    return convert_date_to_utz_with_z(get_date_now_offset(
+        years=years, months=months, days=days, hours=hours))
+
+
+def convert_date_to_utz_with_z(date: datetime) -> str:
+    return date.strftime("%Y-%m-%dT%H:%M:%SZ")
