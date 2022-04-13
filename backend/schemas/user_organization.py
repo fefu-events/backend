1a924c20a34730c82dbc56bb5006ccecda92f3e7
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-13 23:44:35 +1000

    add schemas for user_organizations

---
+++ backend/schemas/user_organization.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+
+class UserOrganizationBase(BaseModel):
+    pass
+
+
+class UserOrganizationCreate(UserOrganizationBase):
+    user_id: int
+    organization_id: int
+
+
+class UserOrganizationCreateWithIsOwner(UserOrganizationCreate):
+    is_owner: bool = Field(default=False)
+
+
+class UserOrganizationInDBBase(UserOrganizationBase):
+    is_owner: bool = Field(default=False)
+
+    class Config:
+        orm_mode = True
