975293208ce30841931b01b3d9c60d230b2161ca
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-14 00:31:48 +1000

    get_by_user_and_organization for crud.user_organization

--- backend/crud/user_organization.py
+++ backend/crud/user_organization.py
@@ -1,3 +1,6 @@
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+
 from backend.crud.base import CRUDBase
 from backend.models.user_organization import UserOrganization
 from backend.schemas.user_organization import UserOrganizationCreate
@@ -6,7 +9,15 @@ from backend.schemas.user_organization import UserOrganizationCreate
 class CRUDUserOrganization(
         CRUDBase[UserOrganization, UserOrganizationCreate, None]
 ):
-    pass
+    def get_by_user_and_organization(
+        self, db: Session, user_id: int, organization_id: int
+    ) -> UserOrganization:
+        return db.query(self.model).\
+            filter(
+                and_(
+                    self.model.user_id == user_id,
+                    self.model.organization_id == organization_id
+                )).first()
 
 
 user_organization = CRUDUserOrganization(UserOrganization)
