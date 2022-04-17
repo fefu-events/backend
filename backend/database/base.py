aab7c0ab8ad7b75283e2145fcc04c541acb568a3
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-17 15:12:14 +1000

    migration. add organization_subscription table #2

--- backend/database/base.py
+++ backend/database/base.py
@@ -1,11 +1,12 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
-from backend.database.base_class import Base  # noqa
-from backend.models.category import Category  # noqa
-from backend.models.event import Event  # noqa
-from backend.models.place import Place  # noqa
-from backend.models.user import User  # noqa
-from backend.models.user_subscription import UserSubscription  # noqa
+from backend.database.base_class import Base # noqa
+from backend.models.category import Category # noqa
+from backend.models.event import Event # noqa
+from backend.models.place import Place # noqa
+from backend.models.user import User # noqa
+from backend.models.user_subscription import UserSubscription # noqa
 from backend.models.participation import Participation # noqa
 from backend.models.organization import Organization # noqa
 from backend.models.user_organization import UserOrganization # noqa
+from backend.models.organization_subscription import OrganizationSubscription # noqa
