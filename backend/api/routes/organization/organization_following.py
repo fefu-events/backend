b245d2cfeb577142b918051246c82e379e737c15
Andrey Varfolomeev <andreika.varfolomeev@yandex.ru>
2022-04-24 19:13:10 +1000

    omission made when rewriting dependencies

--- backend/api/routes/organization/organization_following.py
+++ backend/api/routes/organization/organization_following.py
@@ -25,7 +25,6 @@ router = APIRouter()
     tags=["organization following"],
 )
 def follow_organization(
-    request: Request,
     organization_id: int,
     db=Depends(get_db),
     current_user: UserInDBBase = Depends(get_current_user()),
@@ -33,7 +32,7 @@ def follow_organization(
         Depends(get_organization_by_id_from_path),
 ):
     create_data = {
-        'follower_id': request.state.current_user.id,
+        'follower_id': current_user.id,
         'organization_id': organization_id
     }
     organization_subscription = crud.organization_subscription.\
