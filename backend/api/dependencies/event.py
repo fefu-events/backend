d6c907335c6d98ea8a68949e720b43a39f2abbd0
ada0l <andreika.varfolomeev@yandex.ru>
2022-04-27 03:43:39 +1000

    fix tests

--- backend/api/dependencies/event.py
+++ backend/api/dependencies/event.py
@@ -2,14 +2,14 @@ from fastapi import Depends, HTTPException, Path, status
 
 from backend import crud
 from backend.api.dependencies.database import get_db
-from backend.schemas.event import EventInDBBase
+from backend.models.event import Event
 from backend.resources import strings
 
 
 def get_event_by_id_from_path(
     event_id: int = Path(..., ge=1),
     db=Depends(get_db)
-) -> EventInDBBase:
+) -> Event:
     event = crud.event.get(db, id=event_id)
     if not event:
         raise HTTPException(
--- tests/api/user/test_user_following.py
+++ tests/api/user/test_user_following.py
@@ -10,9 +10,9 @@ from tests.utils import get_ids_ordered
 def test_follow_normal(users, i_user, client_app):
     follower = users[i_user]
     user = users[i_user + 1]
-    response = client_app.post(f"/user/{user.id}/follow",
+    response = client_app.post(f"/user/{user.id}/follow/",
                                headers=follower.get_test_headers())
-    response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data == {"user_id": user.id, "follower_id": follower.id}
 
@@ -20,9 +20,9 @@ def test_follow_normal(users, i_user, client_app):
 def test_follow_yourself(users, client_app):
     follower = users[0]
     user = users[0]
-    response = client_app.post(f"/user/{user.id}/follow",
+    response = client_app.post(f"/user/{user.id}/follow/",
                                headers=follower.get_test_headers())
-    response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     data = response.json()
     assert data['detail'] == strings.UNABLE_TO_FOLLOW_YOURSELF
 
@@ -30,11 +30,11 @@ def test_follow_yourself(users, client_app):
 def test_follow_already(users, client_app):
     follower = users[0]
     user = users[1]
-    client_app.post(f"/user/{user.id}/follow",
+    client_app.post(f"/user/{user.id}/follow/",
                     headers=follower.get_test_headers())
-    response = client_app.post(f"/user/{user.id}/follow",
+    response = client_app.post(f"/user/{user.id}/follow/",
                                headers=follower.get_test_headers())
-    response.status_code == status.HTTP_409_CONFLICT
+    assert response.status_code == status.HTTP_409_CONFLICT
     data = response.json()
     assert data['detail'] == strings.USER_IS_ALREADY_FOLLOWED
 
@@ -43,11 +43,11 @@ def test_follow_already(users, client_app):
 def test_unfollow_normal(users, i_user, client_app):
     follower = users[i_user]
     user = users[i_user + 1]
-    client_app.post(f"/user/{user.id}/follow",
+    client_app.post(f"/user/{user.id}/follow/",
                     headers=follower.get_test_headers())
-    response = client_app.delete(f"/user/{user.id}/unfollow",
+    response = client_app.delete(f"/user/{user.id}/unfollow/",
                                  headers=follower.get_test_headers())
-    response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data["detail"] == strings.USER_IS_NOT_FOLLOWED
 
@@ -56,16 +56,16 @@ def test_unfollow_normal(users, i_user, client_app):
 def test_unfollow_not_followed(users, i_user, client_app):
     follower = users[i_user]
     user = users[i_user + 1]
-    response = client_app.delete(f"/user/{user.id}/unfollow",
+    response = client_app.delete(f"/user/{user.id}/unfollow/",
                                  headers=follower.get_test_headers())
-    response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_409_CONFLICT
     data = response.json()
     assert data["detail"] == strings.USER_IS_NOT_FOLLOWED
 
 
 @parametrize("i_user", range(0, 3))
 def test_empty_followers(users, i_user, client_app):
-    response = client_app.get(f"/user/{users[i_user].id}/followers",
+    response = client_app.get(f"/user/{users[i_user].id}/followers/",
                               headers=users[i_user].get_test_headers())
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
@@ -79,9 +79,9 @@ def test_followers_normal(users, client_app):
         if user_i == user:
             continue
         users_ids.append(user_i.id)
-        client_app.post(f"/user/{user.id}/follow",
+        client_app.post(f"/user/{user.id}/follow/",
                         headers=user_i.get_test_headers())
-    response = client_app.get(f"/user/{user.id}/followers")
+    response = client_app.get(f"/user/{user.id}/followers/")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     users_ids.sort()
@@ -90,7 +90,7 @@ def test_followers_normal(users, client_app):
 
 @parametrize("i_user", range(0, 3))
 def test_empty_following(users, i_user, client_app):
-    response = client_app.get(f"/user/{users[i_user].id}/following",
+    response = client_app.get(f"/user/{users[i_user].id}/following/",
                               headers=users[i_user].get_test_headers())
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
@@ -102,12 +102,12 @@ def test_following(users, client_app):
     for user_i in users:
         if user_i is user:
             continue
-        client_app.post(f"/user/{user.id}/follow",
+        client_app.post(f"/user/{user.id}/follow/",
                         headers=user_i.get_test_headers())
     for user_i in users:
         if user_i is user:
             continue
-        response = client_app.get(f"/user/{user_i.id}/following")
+        response = client_app.get(f"/user/{user_i.id}/following/")
         assert response.status_code == status.HTTP_200_OK
         data = response.json()
         assert get_ids_ordered(data) == [user.id]
